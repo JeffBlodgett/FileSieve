1fd59a951344fa9a3f829922540134c8dee3f088
Dan Bendig <dbendig@gmail.com>
2014-06-29 23:06:46 -0400

    Added HTMLDiffReport stub and DiffReportFactory class. Added code to WindowPlacementPreferences to set the window placement within the screen to ensure that the window does not span the screen and to center the window if no preferences were previously set.

---
+++ src/main/java/FileSieve/Persistence/Reports/HTMLDiffReport.java
@@ -0,0 +1,14 @@
+package FileSieve.Persistence.Reports;
+
+/**
+ * A DiffReport class for outputting HTML reports
+ */
+public class HTMLDiffReport implements DiffReport {
+    public String getReport() {
+        return "";
+    }
+
+    public void save(String savePath) {
+
+    }
+}

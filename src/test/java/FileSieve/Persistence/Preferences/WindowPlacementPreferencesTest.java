f4fc68d190752dc5197db721614b016ce1ab4fcf
Dan Bendig <dbendig@gmail.com>
2014-07-06 18:24:46 -0400

    Changed both Preferences classes to use their their package & class to uniquely store items in preferences. Changed SelectedPaths to save and load multiple reference paths. Added code to set the window location and ensure that the window is within the bounds of a screen. (This needs to be tested more thoroughly but my dev environment only has one screen.) Implemented a basic HTML DiffReport based on the output from a FileDifferentiator object. (As discussed, we will eventually have an object that creates reports based on the results from the UI.) Added code to cleanup the Preferences tests by either setting the values back to their previous state or clearing them if they did not previously exist. Added a unit test for the DiffReport.

--- src/test/java/FileSieve/Persistence/Preferences/WindowPlacementPreferencesTest.java
+++ src/test/java/FileSieve/Persistence/Preferences/WindowPlacementPreferencesTest.java
@@ -6,10 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import javax.swing.JFrame;
+import java.util.prefs.BackingStoreException;
 
 public class WindowPlacementPreferencesTest {
     private WindowPlacementPreferences windowPrefs;
     private JFrame uiWindow;
+    private int originalWidth;
+    private int originalHeight;
+    private int originalLeft;
+    private int originalTop;
+    private boolean hadOriginalPrefs;
+
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+    private static final int DEFAULT_LEFT = 75;
+    private static final int DEFAULT_TOP = 50;
 
     @Before
     public void setup() {
@@ -17,12 +28,29 @@ public class WindowPlacementPreferencesTest {
         uiWindow.setBounds(DEFAULT_LEFT, DEFAULT_TOP, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         uiWindow.setVisible(true);
         windowPrefs = new WindowPlacementPreferences(uiWindow);
+
+        //if the preferences were previously set we need to get the values so we can return them after the tests
+        hadOriginalPrefs = windowPrefs.getPrefsSet();
+        if (hadOriginalPrefs) {
+            originalWidth = windowPrefs.getWindowWidth();
+            originalHeight = windowPrefs.getWindowHeight();
+            originalLeft = windowPrefs.getWindowLeft();
+            originalTop = windowPrefs.getWindowTop();
+        }
+
         windowPrefs.save();
     }
 
     @After
-    public void cleanup() {
-        //TODO: Set preferences back to original values
+    public void cleanup() throws BackingStoreException {
+        if (hadOriginalPrefs) {
+            uiWindow.setBounds(originalLeft, originalTop, originalWidth, originalHeight);
+            uiWindow.setVisible(true);
+            windowPrefs = new WindowPlacementPreferences(uiWindow);
+            windowPrefs.save();
+        } else {
+            windowPrefs.clear();
+        }
     }
 
     @Test
@@ -45,13 +73,4 @@ public class WindowPlacementPreferencesTest {
         Assert.assertEquals(windowPrefs.getWindowLeft(), DEFAULT_LEFT);
     }
 
-    @Test
-    public void getScreenTest() {
-        Assert.assertEquals(windowPrefs.getScreen(), 0);
-    }
-
-    private static final int DEFAULT_WIDTH = 300;
-    private static final int DEFAULT_HEIGHT = 200;
-    private static final int DEFAULT_TOP = 50;
-    private static final int DEFAULT_LEFT = 75;
 }
